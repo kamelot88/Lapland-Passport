799983bfdb8bb5f0ca283d2311afb97b36fdb0c8
kamelot88 <VasilchenkoOV1988@gmail.com>
2021-12-05 14:16:34 +0200

    feat: add table passport

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
   var myAudio1 = new Audio('/audio/metel.mp3');
-  myAudio1.play();
+//   myAudio1.play();
 
-// setTimeout(() => {
-//     myAudio1.volume = '0.1';
-//     myAudio1.play();
-// }, 5);
-// setTimeout();
+setTimeout(() => {
+    myAudio1.volume = '0.1';
+    myAudio1.play();
+    myAudio1.play();
+    myAudio1.play();
+}, 5);
+setTimeout();
